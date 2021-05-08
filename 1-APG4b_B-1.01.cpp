ee38feeef0953dc661a40556d2aefb4b0990b2ed
ネコシャーク02 <82247089+nekoshark02@users.noreply.github.com>
2021-05-08 11:28:36 +0900

    EX1-コードテストと出力の練習

---
+++ 1-APG4b_B-1.01.cpp
@@ -0,0 +1,41 @@
+//APG4bの一番最初の問題「出力とコメント」からやっていきます。「はじめに」は問題がないので、各自で読んでおくだけでいいと思います
+
+/*問題文
+次の出力をするプログラムを書いてください。
+
+実行結果
+こんにちは
+AtCoder
+
+出力の最後（つまりAtCoderの後）に改行する必要があります。注意してください。*/
+
+//サンプルプログラム
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "Hello, world!" << endl;
+  cout << "Hello, AtCoder!" << endl;
+  cout << "Hello, C++!" << endl;
+}
+
+/*このサンプルプログラムのcoutの中にある文字を「こんにちは」「AtCoder」にすればOKです
+注意点
+・出力の最後に改行する必要があります。改行するにはcoutの文の最後に「endl」が必要です。あと、endl（エンド"エル"（L））なので、end1(エンド"いち"(1))にしないように注意し
+ましょう
+・Atcoderの大文字と小文字に注意しましょう。正確に出力させる必要があります
+*/
+
+//ACになったコード例
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "こんにちは" << endl;
+  cout << "AtCoder" << endl;
+  
+}
+
+//ACになりましたか?(´・ω・`)
