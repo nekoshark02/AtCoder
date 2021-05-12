5bb66ed959d1ba4ce821e758e061714d0c247310
ネコシャーク02 <82247089+nekoshark02@users.noreply.github.com>
2021-05-12 22:30:16 +0900

    EX-4「〇年は何年?」

--- 4-APG4b_E-1.04.cpp
+++ 4-APG4b_E-1.04.cpp
@@ -1,1 +1,34 @@
 //四番目の問題「変数と型」です。プログラムでほぼ毎回使うので、すぐに覚えられると思います
+/*問題文
+次のプログラムをコピー＆ペーストして、指定した場所にプログラムを追記することで問題を解いて下さい。*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // 一年の秒数
+  int seconds = 365 * 24 * 60 * 60;
+
+  // 以下のコメント/* */を消して追記する
+  cout << /* 1年は何秒か */ << endl;
+  cout << /* 2年は何秒か */ << endl;
+  cout << /* 5年は何秒か */ << endl;
+  cout << /* 10年は何秒か */ << endl;
+}
+
+/*int型で宣言した変数secondsには「365*24*60*60」という処理が格納されています。このsecondsを使えばいちいち(365*24*60*60)*n(nは年の数)と書かなくても楽に出せて
+ミスが少ないということですね*/
+
+//ACになったコード例
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // 一年の秒数
+  int seconds = 365 * 24 * 60 * 60;
+
+  // 以下のコメント/* */を消して追記する
+  cout <<1*seconds<< endl;
+  cout <<2*seconds<< endl;
+  cout <<5*seconds<< endl;
+  cout <<10*seconds<< endl;
+}
