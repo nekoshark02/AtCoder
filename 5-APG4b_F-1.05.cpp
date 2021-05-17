06370b0fbe21dc7831f411d8f84dac26cee2cc00
ネコシャーク02 <82247089+nekoshark02@users.noreply.github.com>
2021-05-17 22:58:10 +0900

    EX-5「A足すB問題」

---
+++ 5-APG4b_F-1.05.cpp
@@ -0,0 +1,29 @@
+//5番目の問題「A足すB問題」です
+
+/*問題文
+2つの整数A, Bが与えられます。A+Bの計算結果を出力してください。(A+Bの計算結果を出力してください。出力の最後には改行が必要です)
+
+サンプルプログラム*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // ここにプログラムを追記
+}
+
+/*まずA,Bの値をintで宣言してcinで入力、それからA,Bを足し算すればOKです*/
+
+//ACになったコード例
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A,B;
+  cin>>A;
+  cin>>B;
+  
+  cout<<A+B<<endl;
+}
+
